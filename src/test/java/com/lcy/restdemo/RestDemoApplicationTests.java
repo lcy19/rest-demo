244406f46d073b2c73d99ac961571129cf5a7381
lichengyang <lichengyang@comeon.com>
2021-10-23 18:15:13 +0800

    first commit

---
+++ src/test/java/com/lcy/restdemo/RestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcy.restdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
